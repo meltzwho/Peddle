55f0fa36196d9ee591dbedfa8a5601e7d045b4a0
Peter Park <parkdhp@gmail.com>
2018-08-10 11:25:33 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`)
+})
---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: ['babel-polyfill', `${SRC_DIR}/index.jsx`]
+  },
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR
+    }, 
+    module : {
+      rules : [
+        {
+          test : /\.jsx?/,
+          include : SRC_DIR,
+          loader : 'babel-loader',
+          options: { presets: ['react', 'env', 'stage-0'] }
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+  }
+};
