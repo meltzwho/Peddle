b68b4d228a4379d2975b52a3d82a357a2b0ef8f9
Peter Park <parkdhp@gmail.com>
2018-08-14 12:12:05 -0400

    feat(listings): map through props to create listing cards for listings

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import ListingCard from './ListingCard';
 
 class Listings extends Component {
   render() {
     return (
       <div>
         <h2>Listings</h2>
+        {this.props.listings.map(listing => {
+          return (
+            <ListingCard 
+              key={listing.id_listing} 
+              listing={listing}
+            />
+          );
+        })}
       </div>
     );
   }
