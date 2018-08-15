f7adba074c49010cdcd45ac43411c6069f221ef8
Peter Park <parkdhp@gmail.com>
2018-08-14 20:27:36 -0400

    feat(usersController): added method to get user info by id

---
+++ server/controllers/usersController.js
@@ -0,0 +1,15 @@
+const db = require('../models/usersModel');
+
+module.exports = {
+  getUserById: (req, res) => {
+    db.getUserById(req.params.id)
+      .then(data => {
+        console.log('controller: user fetched');
+        res.status(200).json(data);
+      })
+      .catch(e => {
+        console.error('controller: there was an error fetching the items', e);
+        res.status(400).json(e);
+      });
+  }
+};
