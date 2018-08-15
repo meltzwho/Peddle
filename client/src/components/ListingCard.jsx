3c1256008440b69945f43ebf6588b821728497ec
Eric Meltzer <etmeltzer@gmail.com>
2018-08-15 17:44:53 -0400

    Listing card improvements + notif to NavDropdown

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Thumbnail, Image, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const ListingCard = (props) => {
   var listing = [];
@@ -7,16 +8,17 @@ const ListingCard = (props) => {
     listing.push(<p>{info}:{props.listing[info]}</p>)
   }
   return (
-    <Thumbnail style={{height: "325px"}}>
-      {/* {listing} */}
-      <Image alt='' src='https://res.cloudinary.com/teepublic/image/private/s--BbHGObwL--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1467290244/production/designs/566209_1.jpg' style={{width: '50%', "margin-left": "25%"}} thumbnail/>
-      <ListGroup style={{'textAlign': 'center', margin: '5px'}}>
-        <ListGroupItem>{'Title'}: {props.listing.title}</ListGroupItem>
-        <ListGroupItem>{'Price'}: ${props.listing.price}</ListGroupItem>
-        <ListGroupItem>{'Condition'}: {props.listing.condition.toUpperCase()}</ListGroupItem>
-
-      </ListGroup>
-    </Thumbnail>
+    <Link className='test' to={`/listingEntry/${props.listing.id_listing}`}>
+      <Thumbnail>
+        {/* {listing} */}
+        <Image alt='' src='https://pbs.twimg.com/media/CkjFUyTXEAEysBY.jpg' style={{width: '75%', "marginLeft": "12.5%", height: '35vh', objectFit: "scale-down"}} thumbnail/>
+        <ListGroup style={{'textAlign': 'center', "marginTop": '5px'}}>
+          <ListGroupItem>{'Title'}: {props.listing.title}</ListGroupItem>
+          <ListGroupItem>{'Price'}: ${props.listing.price}</ListGroupItem>
+          <ListGroupItem>{'Condition'}: {props.listing.condition.toUpperCase()}</ListGroupItem>
+        </ListGroup>
+      </Thumbnail>
+    </Link>
   );
 };
 
