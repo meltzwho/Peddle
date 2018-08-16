237fff919fac2603cf80f6a61064aa16de5df28f
Peter Park <parkdhp@gmail.com>
2018-08-16 16:55:39 -0400

    fix(paymentController): fixed route to handle id

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const paymentController = require('../controllers/paymentController.js');
 
 // listings route
-router.get('/stripe/:stripeId', paymentController.getAccount);
+router.get('/stripe/id/:stripeId', paymentController.getAccount);
 
 module.exports = router;
