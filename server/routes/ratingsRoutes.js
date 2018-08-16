f1c9ff601c39971cda95ad200f7d458ea21b290c
Peter Park <parkdhp@gmail.com>
2018-08-15 20:26:31 -0400

    fix(ratingsRoutes): added subroute to sepcify userId

--- server/routes/ratingsRoutes.js
+++ server/routes/ratingsRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const ratingsController = require('../controllers/ratingsController');
 
 // listings route
-router.get('/:userId', ratingsController.getRatingsByUserId);
+router.get('/userId/:userId', ratingsController.getRatingsByUserId);
 
 module.exports = router;
