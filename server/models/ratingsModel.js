b8d45e825be1b2a034e630d28b00fcf2c3302651
Peter Park <parkdhp@gmail.com>
2018-08-17 13:30:26 -0400

    feat(ratingsModel): added getFeedbackByUserId

--- server/models/ratingsModel.js
+++ server/models/ratingsModel.js
@@ -18,5 +18,22 @@ module.exports = {
       .catch(e => {
         console.error('there was an error getting the pool connection', e);
       });
-  }
+  },
+  getFeedbackByUserId: (id) => {
+    return db.connect()
+      .then(client => {
+        return client.query(`SELECT * FROM feedback WHERE id_seller=${id}`)
+          .then(res => {
+            client.release();
+            return res.rows;
+          })
+          .catch(e => {
+            client.release();
+            console.log('error fetching listing', e);
+          })
+      })
+      .catch(e => {
+        console.error('there was an error getting the pool connection', e);
+      });
+  },
 };
