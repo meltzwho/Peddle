916350973ebf8ef84f540c911f53c8a87b770df6
Peter Park <parkdhp@gmail.com>
2018-08-17 13:21:26 -0400

    feat(raingsRoutes.js): added get feedback by userId

--- server/routes/ratingsRoutes.js
+++ server/routes/ratingsRoutes.js
@@ -3,5 +3,6 @@ const ratingsController = require('../controllers/ratingsController');
 
 // listings route
 router.get('/userId/:userId', ratingsController.getRatingsByUserId);
+router.get('/feedback/:userId', ratingsController.getFeedbackByUserId)
 
 module.exports = router;
