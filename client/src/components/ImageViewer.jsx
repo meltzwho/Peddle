3a0ed64633f09f96db4aa24cdfd781ad022f3698
Peter Park <parkdhp@gmail.com>
2018-08-18 17:32:30 -0400

    feat(imageViewer.jsx): added default no img available if no images uploaded

--- client/src/components/ImageViewer.jsx
+++ client/src/components/ImageViewer.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 class ImageViewer extends Component {
   render() {
-    if (this.props.images !== undefined) {
+    if (this.props.images === undefined) {
+      return null;
+    } else if (this.props.images.length === 0 ) {
+      return (
+        <Image src='/assets/No-image-available.jpg' alt='no image available' />
+      );
+    } else {
       return (
         <ImageGallery 
           items={this.props.images} 
           showBullets={false}
           showFullscreenButton={true}
           showPlayButton={false}
-          thumbnailPosition={"bottom"}
-          
+          thumbnailPosition={'bottom'}
         />
       );
-    } else {
-      return null;
     }
   }
 }
