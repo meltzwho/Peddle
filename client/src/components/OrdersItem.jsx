33717e77b724b1dfa19a978420b53635d6dbd4e1
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-08-31 13:51:33 -0400

    orders bug fix

--- client/src/components/OrdersItem.jsx
+++ client/src/components/OrdersItem.jsx
@@ -45,7 +45,7 @@ const OrdersItem = (props) => {
                 <Button>View Item</Button>
               </Link>
             </Col>
-            <Col sx={6} sm={6} md={1} style={{margin: "20px"}}>
+            <Col sx={6} sm={6} smOffset={1} md={1} mdOffset={3}>
               {orderCompleteButton}
             </Col>
           </Row>
